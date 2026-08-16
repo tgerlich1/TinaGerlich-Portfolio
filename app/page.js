2db451b4569c55002281de85c62e73b08da4689a
tgerlich1 <96787582+tgerlich1@users.noreply.github.com>
2026-08-16 18:56:12 -0500

    Replace headshot image with a new file name

--- app/page.js
+++ app/page.js
@@ -93,7 +93,7 @@ export default function Home() {
           <div className="hero-photo-frame">
             <img
               className="hero-photo"
-              src="/tina-gerlich-headshot.png"
+              src="/headshot.png"
               alt="Tina Gerlich"
             />
           </div>
